bc098900e4087641972018cfe35dcd8991548a7b
Gabriela <ferblanco1003@gmail.com>
2018-08-22 07:57:25 -0500

    Delete expresiones_out.py

--- arboles_expresiones_archivos/expresiones_out.py
+++
@@ -1,9 +0,0 @@
-def guardar_resultado (resultado):
-    archivo = open("archivo_sal.txt", "r+")
-    contenido = archivo.read()
-    final_archivo = archivo.tell()
-
-    archivo.write(resultado)
-    archivo.seek(final_archivo)
-    nuevo_contenido = archivo.read()
-
